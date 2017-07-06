a0e176fde3c96002dcbaa2bdae9031e26f2a39f2
Ross Stepaniak <ross@maliwan.net>
2017-07-06 14:28:54 +0300

    add basic loading animation

---
+++ Walla/Pods/CBStoreHouseRefreshControl/Class/CBStoreHouseRefreshControl.h
@@ -0,0 +1,45 @@
+//
+//  CBStoreHouseRefreshControl.h
+//  CBStoreHouseRefreshControl
+//
+//  Created by coolbeet on 10/30/14.
+//  Copyright (c) 2014 Suyu Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBStoreHouseRefreshControl : UIView
+
++ (CBStoreHouseRefreshControl*)attachToScrollView:(UIScrollView *)scrollView
+                                           target:(id)target
+                                    refreshAction:(SEL)refreshAction
+                                            plist:(NSString *)plist;
+
++ (CBStoreHouseRefreshControl*)attachToScrollView:(UIScrollView *)scrollView
+                                           target:(id)target
+                                    refreshAction:(SEL)refreshAction
+                                            plist:(NSString *)plist
+                                            color:(UIColor*)color
+                                        lineWidth:(CGFloat)lineWidth
+                                       dropHeight:(CGFloat)dropHeight
+                                            scale:(CGFloat)scale
+                             horizontalRandomness:(CGFloat)horizontalRandomness
+                          reverseLoadingAnimation:(BOOL)reverseLoadingAnimation
+                          internalAnimationFactor:(CGFloat)internalAnimationFactor;
++ (CBStoreHouseRefreshControl*)attachToView:(UIView *)view
+                                     target:(id)target
+                              refreshAction:(SEL)refreshAction
+                                      plist:(NSString *)plist
+                                      color:(UIColor*)color
+                                  lineWidth:(CGFloat)lineWidth
+                                 dropHeight:(CGFloat)dropHeight
+                                      scale:(CGFloat)scale
+                       horizontalRandomness:(CGFloat)horizontalRandomness
+                    reverseLoadingAnimation:(BOOL)reverseLoadingAnimation
+                    internalAnimationFactor:(CGFloat)internalAnimationFactor;
+
+
+- (void)start;
+- (void)stop;
+
+@end
